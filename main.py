aae529c68089c4fed0bb8de0d56e364c17b14f3d
rauldangio <jojofagqwrt@gmail.com>
2023-04-03 21:13:30 -0300

    parsing do json

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import json
+
 import requests
 
+URL = "https://restcountries.com/v3.1/all"
+URL_NAME = "https://restcountries.com/v3.1/name/brazil"
+resposta = requests.get(URL_NAME)
 
-URL = "https://api.countrylayer.com/v2/all"
-resposta = requests.get(URL)
-print(resposta.content)
+paises = json.loads(resposta.text)
+
+for pais in paises:
+    print(pais['capital'])
