f264c6cafe03954849297e6dcb2c358de646c8aa
rauldangio <jojofagqwrt@gmail.com>
2023-04-05 00:27:27 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 import json
+from argparse import ArgumentParser
 
-import requests
+import requests as re
 
 URL = "https://restcountries.com/v3.1/all"
-URL_NAME = "https://restcountries.com/v3.1/name/brazil"
-resposta = requests.get(URL_NAME)
+URL_NAME = "https://restcountries.com/v3.1/name/"
 
-paises = json.loads(resposta.text)
 
-for pais in paises:
-    print(pais['capital'])
+def reqWeb(url):
+    try:
+        req = re.get(url)
+        if req.status_code == 200:
+            return req.text
+    except Exception as error:
+        print(error)
+    
+    return None
+
+def parsing(text):
+    try:
+        return json.loads(text) 
+    except Exception as error:
+        print(error)
+
+
+def language(country):
+    resp = re.get(URL_NAME + country)
+    list_country = json.loads(resp.text)
+    for country in list_country:
+        print(f"{country['name']['common']} --> {country['languages']}")
+
+
+def capital(country):
+    resp = re.get(URL_NAME + country)
+    list_country = json.loads(resp.text)
+    for country in list_country:
+        print(f"{country['name']['common']} --> {country['capital'][0]}")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-l", help="return the language of the country")
+    parser.add_argument("-c", help="return the capital of the country")
+    args = parser.parse_args()
+    if(args.l):
+        print("LANGUAGES")
+        language(args.l)
+        print()
+        print("----------------------")
+    if(args.c):
+        print("CAPITAL")
+        capital(args.c)
+        print()
+        print("----------------------")
+    
